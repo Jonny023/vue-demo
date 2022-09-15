bb049059aa7f68c13da38c1aa69aa22804edc2c1
Jonny <342418262@qq.com>
2022-09-15 17:26:10 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Hello from '@/components/hello'
+import Hello from '@/components/Hello'
 
 Vue.use(Router)
 
