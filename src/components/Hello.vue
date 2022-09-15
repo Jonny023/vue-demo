d6d4c4d0422312b53255f37287a8f9f2cc72d358
Jonny <342418262@qq.com>
2022-09-15 16:52:16 +0800

    demo

---
+++ src/components/Hello.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Hello, 你好，{{ name }}</h1>
+        <el-switch v-model="value" active-color="#13ce66" inactive-color="#ff4949">
+        </el-switch>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            name: '张三',
+            value: true
+        }
+    }
+}
+</script>
+
+<style>
+</style>
